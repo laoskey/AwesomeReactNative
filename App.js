dc0ecfd83fa2151f42610cdea51c42e5945cd864
观澜听雨 <tangxiaochuane@yeah.net>
2024-12-10 16:26:23 +0800

    add the new component

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+import Inde from './components/Inde';
+
+function App() {
+  return (
+    <ScrollView>
+      <Text>Hello React</Text>
+      <Inde />
+    </ScrollView>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+
+export default App;
---
+++ components/Inde.jsx
@@ -0,0 +1,8 @@
+import {Text} from 'react-native';
+import React from 'react';
+
+function Inde() {
+  return <Text>Index for the AwesomeReactNative Project</Text>;
+}
+
+export default Inde;
